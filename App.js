fc51c9c2a94705797adadf70ade8fef542137152
nikantonelli <nik@nikantonelli.com>
2015-01-14 14:07:33 +0000

    Added LBAPI fetch of Features

--- App.js
+++ App.js
@@ -17,8 +17,9 @@ Ext.define('CustomApp', {
     ],
 
     piStr: "",
+    objStr: "",
 
-    portfolioIds: [],
+    portfolioIds: null,
     workItems: [],
     chsrDlg: null,
 
@@ -28,7 +29,13 @@ Ext.define('CustomApp', {
             {
                 name: 'epicIDs',
                 xtype: 'rallytextfield',
-                label: " Space separated list of Portfolio Items to include"
+                label: " Space separated list of Portfolio Items",
+                readOnly: true
+            },
+            {
+                name: 'epicOBJs',
+                xtype: 'rallytextfield',
+                hidden: true
             }
         ];
 
@@ -42,32 +49,45 @@ Ext.define('CustomApp', {
 
     _findAllLowestLevelPIs: function(app) {
 
+        var lowestPiName = "";
+
         //Find the lowest level PI type from the store
-        if (app.piStore){
-            
+        if (app.typeStore){
+            _.each(app.typeStore.data.items, function(item) {
+                if (item.get('Ordinal') === 0){
+                    lowestPiName = item.get('TypePath');
+                }
+            });
         }
         else
         {
-            Rally.ui.notify.Notifier.show({message: 'Portfolio Type Hierarchy not available. Try and reload page');
+            Rally.ui.notify.Notifier.show({message: 'Portfolio Type Hierarchy not available. Try and reload page'});
         }
 
         //In the beginning, we have all the items in piStr of type Ext.getCmp('typeSelector').rawValue
-        var piList = app.piStr.split(" ");
+        var objList = [];
+
+        _.each( app.objStr.split(" "), function(str) {
+        
+            objList.push(Number(str));
+        });
+
         var piType = Ext.getCmp('typeSelector').rawValue;
 
-        //Use the lookback API as it allows us to use the '$in' operator
+        //Use the lookback API as it allows us to use the '$in' operator and gets
         var piStore = Ext.create('Rally.data.lookback.SnapshotStore', {
             autoLoad: true,
 
             fetch: ['FormattedID', 'Name', 'Children'],
+            hydrate: ['FormattedID'],
             filters:  [ {
-                            property: 'FormattedID',
+                            property: '_ItemHierarchy',
                             operator: '$in',
-                            value: piList
+                            value: objList
                         },
                         {
                             property: '_TypeHierarchy',
-                            value: 'PortfolioItem/Feature'
+                            value: lowestPiName
                         },
                         {
                             property: "__At",
@@ -79,9 +99,10 @@ Ext.define('CustomApp', {
 
                     //Add all the items that are lowest level PIs to the collection
 
-                    _.each(data, function(item) {
-                    debugger;
-                    });
+                    app.portfolioIds = data;
+
+                    //Update all the containers with the details in.
+                    app._updateDetailsPanes(app);
                 }
             }
         });
@@ -103,15 +124,34 @@ Ext.define('CustomApp', {
             app.chsrDlg.destroy();
         }
 
+        artifactType = 'PortfolioItem';
+        title = "Choose ";
+
+        if ( Ext.getCmp('ignoreType').value === false)
+        {
+            artifactType += '/' + Ext.getCmp('typeSelector').rawValue;
+            title += Ext.getCmp('typeSelector').rawValue;
+        }
+
         app.chsrDlg = Ext.create('Rally.ui.dialog.SolrArtifactChooserDialog', {
-            artifactTypes: 'PortfolioItem/' + Ext.getCmp('typeSelector').rawValue,
+            artifactTypes: artifactType,
             autoShow: true,
             height: 400,
-            title: 'Choose ' + Ext.getCmp('typeSelector').rawValue + ' item(s)',
+            title: title  + ' item(s)',
             multiple: true,
 
             storeConfig: {
-                fetch:['UserStories','Name','FormattedID','TypePath','ObjectID','PortfolioItemType']
+                fetch:['UserStories','Name','FormattedID','TypePath','ObjectID','PortfolioItemType'],
+                sorters: [
+                    {
+                        property: 'PortfolioItemType',
+                        direction: 'DESC'
+                    },
+                    {
+                        property: 'FormattedID',
+                        direction: 'ASC'
+                    }
+                ]
             },
 
             listeners: {
@@ -119,25 +159,30 @@ Ext.define('CustomApp', {
 
                     //Refresh the list of stuff we are looking at
                     var itmStr = "";
+                    var objStr = "";
 
                     selectedRecord.forEach( function(record) {
                         delim = (itmStr === "")?"":" ";
                         itmStr += delim + record.get('FormattedID'); //Add a space as delimiter
+                        objStr += delim + record.get('ObjectID'); //Add a space as delimiter
                     });
 
                     //Save list in the settings field for next time
                     app.updateSettingsValues( {
                         settings: {
-                            epicIDs: itmStr
+                            epicIDs: itmStr,
+                            epicOBJs: objStr
                         }
                     });
                     app.piStr = itmStr;
+                    app.objStr = objStr;
 
                     //Refresh the text box in the UI
                     app._updatePortfolioItemList(app);
 
-                    //Update all the containers with the details in.
-                    app._updateDetailsPanes(app);
+                    //Fetch a new list of lowest level PIs for the items chosen
+                    app._findAllLowestLevelPIs(app);
+
 
                 }
             }
@@ -150,8 +195,7 @@ Ext.define('CustomApp', {
 
     _updateDetailsPanes: function(app){
 
-        //Fetch a new list of lowest level PIs for the items chosen
-        app._findAllLowestLevelPIs(app);
+debugger;
 
         //Add the first chart after the header.
         app._piBurnupChart(app);
@@ -164,15 +208,15 @@ Ext.define('CustomApp', {
             Ext.getCmp('selectionFromSettings').destroy();
         }
         var txtMsg = Ext.create( 'Ext.form.field.Text', {
-                    fieldLabel: 'Current Selected Items',
+                    fieldLabel: 'Current Selected',
                     id: 'selectionFromSettings',
                     grow: true,
                     value: app.piStr,
                     border: 0,
-                    margin: '0 0 0 10',
+                    margin: 10,
                     readOnly: true
                 });
-        Ext.getCmp('headerBox').insert(1, txtMsg);
+        Ext.getCmp('headerBox').add(txtMsg);
     },
 
     typeStore: null,
@@ -182,20 +226,35 @@ Ext.define('CustomApp', {
         var app = this;
 
         //Check to see if there are some items defined in the settings page.
-        //If not, add the button to be a selector
-        app.piStr =  app.getSetting('epicIDs');
+        ids = app.getSetting('epicIDs');
+        objs = app.getSetting('epicOBJs');
+
+        if ( ids && objs ) {
+            app.piStr = ids ; //user visible
+            app.objStr = objs ; //Hidden for our use
+        }
 
         //The typeSelector will give us a store with the portfolio hierarchy in it.
         //We can work off the back of that
+
+        var ignoreType = Ext.create( 'Rally.ui.CheckboxField', {
+            fieldLabel: 'Ignore Type',
+            id: 'ignoreType',
+            value: false,
+            margin: 10
+        });
+
+        Ext.getCmp('headerBox').add(ignoreType);
 
         var typeSelect = Ext.create( 'Rally.ui.combobox.PortfolioItemTypeComboBox', {
             id: 'typeSelector',
             margin: 10,
             listeners: {
+
                 ready: function() {
                     app.typeStore = Ext.getCmp('typeSelector').store;
                     //Add a button so that we can choose something of type in typeSelector
-                    Ext.getCmp('headerBox').add( { xtype: 'rallybutton',
+                    Ext.getCmp('headerBox').insert(2, { xtype: 'rallybutton',
                         margin: 10,
                         id: 'doitButton',
                         text: 'Select Item(s)',
@@ -203,13 +262,21 @@ Ext.define('CustomApp', {
                             app._doArtifactChooserDialog(app);
                         }
                     });
+
+                    //When the typeSelector is loaded, we have all the type information so that we can now
+                    //update the details panes as well
+
+                    if (app.piStr){ //When we first come in, this is not set and the user needs to select something
+                        app._updatePortfolioItemList(app);
+                        app._findAllLowestLevelPIs(app);
+                    }
                 }
             }
         });
 
-        Ext.getCmp('headerBox').add(typeSelect);
+        Ext.getCmp('headerBox').insert(0, typeSelect);
 
-        app._updatePortfolioItemList(app);
+
 
     }
 });
